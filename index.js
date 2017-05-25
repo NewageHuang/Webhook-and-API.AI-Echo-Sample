6502bc718a627db6f1fcb8858182cf9e49715a0b
NewageHuang <nathan.huang@pwc.com>
2017-05-25 18:47:54 -0400

    sdsd

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ restService.post('/echo', function(req, res) {
       // });
       client.messages.create({
           to: "+16467523706",
-          from: "+16467523706",
+          from: "+15005550006",
           body: "Hello from Google Home!",
           mediaUrl: "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
       }, function(err, message) {
