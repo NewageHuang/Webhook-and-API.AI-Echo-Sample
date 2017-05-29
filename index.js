ea14f7e961c5548cc4b8796abc744c05a7e45a97
NewageHuang <nathan.huang@pwc.com>
2017-05-29 15:21:33 +0800

    sdsds

--- index.js
+++ index.js
@@ -29,106 +29,126 @@ io.on('connection', (socket) => {
 
 
 
-// restService.post('/echo', function(req, res) {
-//     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-//     if(new RegExp('slide').test(speech)||new RegExp('ppt').test(speech)||new RegExp('presentation').test(speech)){
-//       speech = 'opening the slides...';
-//       io.emit('openslide', new Date().toTimeString());
-//     }else if (new RegExp('next').test(speech)) {
-//       speech = 'here is the next page...';
-//       io.emit('nextpage', new Date().toTimeString());
-//     }else if (new RegExp('back').test(speech) || new RegExp('previous').test(speech)|| new RegExp('last page').test(speech)) {
-//       speech = 'here you go';
-//       io.emit('back', new Date().toTimeString());
-//     }else if (new RegExp('message').test(speech)) {
-//       // client.messages.create({
-//       //     to: "+16467523706",
-//       //     from: "+16467523706",
-//       //     body: "Hello from Google Home!",
-//       //     mediaUrl: "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
-//       // }, function(err, message) {
-//       //     console.log('twilio message error:'+err+' message id:'+message);
-//       // });
-//       client.messages.create({
-//           to: "+16467523706",
-//           from: "+18057197602",
-//           body: "Hello from Google Home!",
-//           mediaUrl: "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
-//       }, function(err, message) {
-//           console.log(Object.keys(err));
-//           //console.log(message.sid);
-//           console.log('twilio message '+err.message);
-//           console.log('twilio message '+err.code);
-//       });
-//       speech = 'messages sent';
-//       io.emit('message', new Date().toTimeString());
-//     }
+restService.post('/echo', function(req, res) {
+    var paras = req.body.result && req.body.result.parameters;
+    var speech = "Seems like some problem. Speak again.";
+    if(paras){
+      if(paras.open){
+        speech = 'opening the slides...';
+        io.emit('openslide', new Date().toTimeString());
+      }else if(paras.next){
+        speech = 'here is the next page...';
+        io.emit('nextpage', new Date().toTimeString());
+      }else if(paras.back){
+        speech = 'here you go';
+        io.emit('back', new Date().toTimeString());
+      }else if(paras.message){
+        client.messages.create({
+            to: "+16467523706",
+            from: "+18057197602",
+            body: "Hello from Google Home!",
+            mediaUrl: "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
+        }, function(err, message) {
+            console.log(Object.keys(err));
+            //console.log(message.sid);
+            console.log('twilio message '+err.message);
+            console.log('twilio message '+err.code);
+        });
+        speech = 'messages sent';
+        io.emit('message', new Date().toTimeString());
+      }
+    }
+    // var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    // if(new RegExp('slide').test(speech)||new RegExp('ppt').test(speech)||new RegExp('presentation').test(speech)){
+    //   speech = 'opening the slides...';
+    //   io.emit('openslide', new Date().toTimeString());
+    // }else if (new RegExp('next').test(speech)) {
+    //   speech = 'here is the next page...';
+    //   io.emit('nextpage', new Date().toTimeString());
+    // }else if (new RegExp('back').test(speech) || new RegExp('previous').test(speech)|| new RegExp('last page').test(speech)) {
+    //   speech = 'here you go';
+    //   io.emit('back', new Date().toTimeString());
+    // }else if (new RegExp('message').test(speech)) {
+    //   client.messages.create({
+    //       to: "+16467523706",
+    //       from: "+18057197602",
+    //       body: "Hello from Google Home!",
+    //       mediaUrl: "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
+    //   }, function(err, message) {
+    //       console.log(Object.keys(err));
+    //       //console.log(message.sid);
+    //       console.log('twilio message '+err.message);
+    //       console.log('twilio message '+err.code);
+    //   });
+    //   speech = 'messages sent';
+    //   io.emit('message', new Date().toTimeString());
+    // }
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
+});
+
+
+// restService.post('/open', function(req, res) {
+//     //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.open ? req.body.result.parameters.open : "Seems like some problem. Speak again."
+//     var speech_open = 'okay...opening the slides...';
+//     io.emit('openslide', new Date().toTimeString());
+//
 //     return res.json({
-//         speech: speech,
-//         displayText: speech,
+//         speech: speech_open,
+//         displayText: speech_open,
 //         source: 'webhook-echo-sample'
 //     });
 // });
-
-
-restService.post('/open', function(req, res) {
-    //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.open ? req.body.result.parameters.open : "Seems like some problem. Speak again."
-    var speech_open = 'okay...opening the slides...';
-    io.emit('openslide', new Date().toTimeString());
-
-    return res.json({
-        speech: speech_open,
-        displayText: speech_open,
-        source: 'webhook-echo-sample'
-    });
-});
-
-restService.post('/next', function(req, res) {
-    //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.next ? req.body.result.parameters.next : "Seems like some problem. Speak again."
-    var speech_next = 'okay...here is the next page...';
-    io.emit('nextpage', new Date().toTimeString());
-
-    return res.json({
-        speech: speech_next,
-        displayText: speech_next,
-        source: 'webhook-echo-sample'
-    });
-});
-
-restService.post('/back', function(req, res) {
-    //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.back ? req.body.result.parameters.back : "Seems like some problem. Speak again."
-    var speech_back = 'here you go';
-    io.emit('back', new Date().toTimeString());
-
-    return res.json({
-        speech: speech_back,
-        displayText: speech_back,
-        source: 'webhook-echo-sample'
-    });
-});
-
-restService.post('/message', function(req, res) {
-    //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.message ? req.body.result.parameters.message : "Seems like some problem. Speak again."
-    var speech_msg = 'messages sent';
-    io.emit('message', new Date().toTimeString());
-
-    client.messages.create({
-       to: "+16467523706",
-         from: "+18057197602",
-         body: "Hello from Google Home!",
-         mediaUrl: "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
-     }, function(err, message) {
-         console.log(Object.keys(err));
-         console.log('twilio message '+err.message);
-         console.log('twilio message '+err.code);
-    });
-
-    return res.json({
-        speech: speech_msg,
-        displayText: speech_msg,
-        source: 'webhook-echo-sample'
-    });
-});
+//
+// restService.post('/next', function(req, res) {
+//     //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.next ? req.body.result.parameters.next : "Seems like some problem. Speak again."
+//     var speech_next = 'okay...here is the next page...';
+//     io.emit('nextpage', new Date().toTimeString());
+//
+//     return res.json({
+//         speech: speech_next,
+//         displayText: speech_next,
+//         source: 'webhook-echo-sample'
+//     });
+// });
+//
+// restService.post('/back', function(req, res) {
+//     //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.back ? req.body.result.parameters.back : "Seems like some problem. Speak again."
+//     var speech_back = 'here you go';
+//     io.emit('back', new Date().toTimeString());
+//
+//     return res.json({
+//         speech: speech_back,
+//         displayText: speech_back,
+//         source: 'webhook-echo-sample'
+//     });
+// });
+//
+// restService.post('/message', function(req, res) {
+//     //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.message ? req.body.result.parameters.message : "Seems like some problem. Speak again."
+//     var speech_msg = 'messages sent';
+//     io.emit('message', new Date().toTimeString());
+//
+//     client.messages.create({
+//        to: "+16467523706",
+//          from: "+18057197602",
+//          body: "Hello from Google Home!",
+//          mediaUrl: "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
+//      }, function(err, message) {
+//          console.log(Object.keys(err));
+//          console.log('twilio message '+err.message);
+//          console.log('twilio message '+err.code);
+//     });
+//
+//     return res.json({
+//         speech: speech_msg,
+//         displayText: speech_msg,
+//         source: 'webhook-echo-sample'
+//     });
+// });
 
 restService.post('/slack-test', function(req, res) {
 
